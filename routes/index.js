980d394beee6396532e0d1f7cf607d9ff7ac9d86
Nazim <nazim.messaoudene@pm.me>
2023-07-05 13:38:30 +0200

    v2

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 let API_KEY = process.env.api_key
 
 router.get('/movies', (req, res) => {
-    fetch (`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
+    fetch (`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.api_key}`)
     .then (response => response.json())
   //.then (data => console.log(data));
   .then (data => res.json({ movies: data.results }));
